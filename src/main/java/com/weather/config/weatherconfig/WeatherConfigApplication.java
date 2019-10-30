713e80892e5dbb1c1f83ea13174e71d56f9ffd6a
Miguel Angel Dominguez Espinoza <miguel.dominguez@globant.com>
2019-10-30 17:06:57 -0600

    Initial commit

---
+++ src/main/java/com/weather/config/weatherconfig/WeatherConfigApplication.java
@@ -0,0 +1,13 @@
+package com.weather.config.weatherconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/config/weatherconfig/WeatherConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.config.weatherconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
