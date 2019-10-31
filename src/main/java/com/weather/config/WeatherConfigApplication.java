bf6712affe502432519307cd2cf151231ff549bb
Miguel Angel Dominguez Espinoza <miguel.dominguez@globant.com>
2019-10-31 17:17:13 -0600

    Added spring cloud config server properties

--- src/main/java/com/weather/config/WeatherConfigApplication.java
+++ src/main/java/com/weather/config/WeatherConfigApplication.java
@@ -2,10 +2,8 @@ package com.weather.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class WeatherConfigApplication {
 
 	public static void main(String[] args) {
