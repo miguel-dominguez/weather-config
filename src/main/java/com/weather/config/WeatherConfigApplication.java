7c2b4742cf6c41a1156194c1411d93df0aa03155
Miguel Angel Dominguez Espinoza <miguel.dominguez@globant.com>
2019-11-01 13:20:54 -0600

    Updated annotations

--- src/main/java/com/weather/config/WeatherConfigApplication.java
+++ src/main/java/com/weather/config/WeatherConfigApplication.java
@@ -1,13 +1,11 @@
 package com.weather.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableConfigServer
+@SpringBootApplication
 public class WeatherConfigApplication {
 
 	public static void main(String[] args) {
